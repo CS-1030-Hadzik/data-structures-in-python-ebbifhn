a1a3a2c07ca7bef9cea1fcf48fda71dfbabe543f
ebbifhn <fhnebbi@gmail.com>
2022-12-01 18:37:00 -0700

    array 2

--- array_1.py
+++ array_1.py
@@ -26,9 +26,26 @@ cars.pop(4)
 #`problem 11. print the array to the console`
 print(cars)
 #`problem 12. Sort the array in ascending order`
-
+cars.sort()
 #`problem 13. print the array to the console`
-
+print(cars)
 #`problem 14. Sort the array in descending order`
-
+cars.reverse
 #`problem 15. print the array to the console`
+print(cars)
+#`problem 16. create a variable called my_array_length with a value of the cars array length (spelling, capitilization, and spaces matter)`
+my_array_length = len(cars)
+#`problem 17. create a variable called array_string with a value of 'The length of my array is ' (spelling, capitilization, and spaces matter)`
+array_string = 'The length of my array is '
+#`problem 18. print array_string concatenated with my_array_length to the console.`
+print(array_string + str(my_array_length))
+
+# for each
+# carrs --> plural
+# car --> singular -- one element
+cars.append("Kia")
+print('===============')
+element = 1
+for car in cars: # len of list for (i=0, i<cars.len, i++)
+    print(element, car)
+    element += 1 # element = element + 1
